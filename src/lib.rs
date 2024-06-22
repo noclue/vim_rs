77e014132b1654f14f7b62dd3b21ff3d9f719d3f
Kiril Karaatanassov <kkaraatanassov@vmware.com>
2024-06-22 21:03:12 +0300

    factor out spec specific code

--- src/lib.rs
+++ src/lib.rs
@@ -1,214 +1,27 @@
 use convert_case::{Case, Casing};
 use openapiv3::{ReferenceOr, Schema, SchemaKind};
 use printer::Printer;
-use std::io::Read;
-use std::collections::HashMap;
+use check_keyword::CheckKeyword;
+use spec::{APISpec, get_parent_schema, parse_schema_from_reference};
 
 mod check_keyword;
-
-use check_keyword::CheckKeyword;
-
-
-
 mod printer;
+mod spec;
 
 // Error is an error type for Project.
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-    #[error("Cannot read file")]
-    FileIOError(#[from] std::io::Error),
-    #[error("Cannot parse JSON")]
-    JSONError(#[from] serde_json::Error),
-    #[error("Reference `{}` cannot be resolved", .0)]
-    NotFound(String),
-    #[error("Cycle detected in reference `{}`", .0)]
-    Cycle(String),
-    #[error("Invalid reference format `{}`", .0)]
-    InvalidReference(String),
     #[error("Unsupported type `{}`", .0)]
     UnsupportedType(String),
-    #[error("Invalid schema `{}`", .0)]
-    InvalidApiSpecs(String),
     #[error("Printer error: {0}")]
     WriteError(#[from] printer::Error),
+    #[error("Spec error: {0}")]
+    SpecError(#[from] spec::Error),
 }
 
 // Result is a type alias for handling errors.
 pub type Result<T> = std::result::Result<T, Error>;
 
-/// An APISpec manages a compilation uint of OpenAPI specifications. At present
-/// one is supported. It is created from a file and additional files may be
-/// loaded to resolve references.
-pub struct APISpec {
-    spec: openapiv3::OpenAPI,
-    children: HashMap<String, Vec<String>>,
-}
-
-impl APISpec {
-    /// Create a new project from a file.
-    pub fn new(file: &str) -> Result<APISpec> {
-        let openapi = load_openapi(file)?;
-        let mut self_ = APISpec { spec: openapi, children: HashMap::new()};
-        self_.children = self_.resolve_schema_hierarchy()?;
-        Ok(self_)
-    }
-
-    pub fn resolve_discriminator_by_name(&self, schema_name: &str) -> Result<String> {
-        let schema_or_ref = self.get_schema(schema_name)?;
-        let ReferenceOr::Item(schema) = schema_or_ref else {
-            return Err(Error::InvalidReference(format!("{} is not a schema", schema_name)));
-        };
-        self.resolve_discriminator(schema_name, schema)
-    }
-
-    pub fn resolve_discriminator(&self, schema_name: &str, schema: &Schema) -> Result<String> {
-        let Some(discriminator) = &schema.schema_data.discriminator else {
-            let Some(parent) = get_parent_schema(schema)? else {
-                return Err(Error::InvalidApiSpecs(format!("{} does not have a discriminator", schema_name)));
-            };
-            return self.resolve_discriminator_by_name(&parent);
-        };
-        Ok(discriminator.property_name.clone())
-    }
-
-    /// Resolve schema by schema name.
-    /// Get Schema by schema name. If the schema is not found return NotFound error.
-    /// If there is cycle in the reference, return Cycle error.
-    /// If the schema is a Reference, resolve it recursively and return the referenced Schema.
-    /// If the schema is a Schema, return the Schema.
-    /// If the schema is not found return NotFound error.
-    pub fn resolve_schema_by_name(&self, schema_name: &str) -> Result<&Schema> {
-        let schema = self.get_schema(schema_name)?;
-        self.resolve_schema(schema)
-    }
-
-    /// Get Schema by schema name. If the schema is not found return NotFound error.
-    /// This method does not resolve the schema if it is a Reference.
-    pub fn get_schema(&self, schema_name: &str) -> Result<&ReferenceOr<Schema>> {
-        let Some(components) = self.spec.components.as_ref() else {
-            return Err(Error::InvalidApiSpecs("Components not found".to_string()));
-        };
-        match components.schemas.get(schema_name) {
-            Some(schema) => Ok(schema),
-            None => Err(Error::NotFound(schema_name.to_string())),
-        }
-    }
-
-    /// Get ReferenceOr<Schema> and return the Schema if it is a Schema.
-    /// If it is a Reference, resolve it recursively and return the referenced Schema.
-    /// If the reference is not found return NotFound error.
-    /// If there is cycle in the reference, return Cycle error.
-    pub fn resolve_schema<'a>(&'a self, item: &'a ReferenceOr<Schema>) -> Result<&'a Schema> {
-        self.resolve_internal(item, &mut vec![])
-    }
-
-    pub fn resolve_boxed_schema<'a>(
-        &'a self,
-        item: &'a ReferenceOr<Box<Schema>>,
-    ) -> Result<&'a Schema> {
-        match item {
-            ReferenceOr::Item(schema) => Ok(schema),
-            ReferenceOr::Reference { reference } => self.resolve_reference(reference, &mut vec![]),
-        }
-    }
-
-    fn resolve_internal<'a>(
-        &'a self,
-        item: &'a ReferenceOr<Schema>,
-        stack: &mut Vec<String>,
-    ) -> Result<&'a Schema> {
-        match item {
-            ReferenceOr::Item(schema) => Ok(schema),
-            ReferenceOr::Reference { reference } => self.resolve_reference(reference, stack),
-        }
-    }
-
-    fn resolve_reference<'a>(
-        &'a self,
-        reference: &String,
-        stack: &mut Vec<String>,
-    ) -> Result<&Schema> {
-        let schema_name = parse_schema_from_reference(reference)?;
-        if schema_name.len() == reference.len() {
-            return Err(Error::InvalidReference(reference.to_string()));
-        }
-        if stack.contains(&schema_name.to_string()) {
-            return Err(Error::Cycle(format!(
-                "{}, {}",
-                stack.join(", "),
-                reference.to_string()
-            )));
-        }
-        stack.push(schema_name.to_string());
-        let ref_or_schema = self.get_schema(schema_name)?;
-        let schema = self.resolve_internal(ref_or_schema, stack)?;
-        Ok(schema)
-    }
-
-    /// Get child schemas of the given schema.
-    /// If the schema has child schemas, return the child schemas.
-    /// If the schema does not have child schemas, return an empty vector.
-    /// If the schema is not found, return NotFound error.
-    pub fn get_children(&self, schema_name: &str) -> Result<&Vec<String>> {
-        match self.children.get(schema_name) {
-            Some(children) => Ok(children),
-            None => Err(Error::NotFound(schema_name.to_string())),
-        }
-    }
-
-    /// Resolve hierarchy of schemas. Returns a Map of schema names to their children schema names.
-    fn resolve_schema_hierarchy(
-        &self
-    ) -> Result<std::collections::HashMap<String, Vec<String>>> {
-        let mut hierarchy = std::collections::HashMap::new();
-
-        for (name, schema) in self.spec.components.as_ref().unwrap().schemas.iter() {
-            let schema = self.resolve_schema(schema)?;
-            let parent = get_parent_schema(schema)?;
-            if let Some(parent_name) = parent {
-                let child_hierarchy = hierarchy.entry(parent_name).or_insert(vec![]);
-                child_hierarchy.push(name.to_string());
-            }
-            hierarchy.entry(name.to_string()).or_insert(vec![]);
-        }
-        Ok(hierarchy)
-    }
-
-}
-
-/// Get parent schema name Some(String) from the "allOf" field if there is exactly one reference value.
-/// If there are multiple values, no value or Schema value return None.
-pub fn get_parent_schema(schema: &Schema) -> Result<Option<String>> {
-    let all_of = match &schema.schema_kind {
-        SchemaKind::Any(any_schema) => &any_schema.all_of,
-        SchemaKind::AllOf { all_of } => all_of,
-        _ => return Ok(None),
-    };
-    if all_of.len() == 1 {
-        if let ReferenceOr::Reference { reference } = &all_of[0] {
-            let schema_name = parse_schema_from_reference(reference)?;
-            return Ok(Some(schema_name.to_string()));
-        }
-    }
-    Ok(None)
-}
-
-fn parse_schema_from_reference(reference: &String) -> Result<&str> {
-    if !reference.starts_with("#/components/schemas/") {
-        return Err(Error::InvalidReference(reference.to_string()));
-    }
-    let schema_name = reference.trim_start_matches("#/components/schemas/");
-    Ok(schema_name)
-}
-
-/// Load an OpenAPI specification from a file.
-fn load_openapi(file: &str) -> Result<openapiv3::OpenAPI> {
-    let mut file = std::fs::File::open(file)?;
-    let mut data = String::new();
-    file.read_to_string(&mut data)?;
-    let openapi: openapiv3::OpenAPI = serde_json::from_str(&data)?;
-    Result::Ok(openapi)
-}
 
 /// Print hierarchy of schemas. Prints ASCII art hirarchy of schemas starting from the given schema.
 /// If the schema has child schemas, they will be printed on new line with increased indentation.
@@ -499,7 +312,7 @@ mod tests {
         let project = load_openapi();
         let file = std::fs::File::create("output.rs").expect("Could not create file");
         let mut printer = printer::FilePrinter::new(file, None, None);
-        let comps = project.spec.components.as_ref().unwrap();
+        let comps = project.get_components().unwrap();
         for (name, _) in comps.schemas.iter() {
             generate_rust_code(&project, name.to_string(), &mut printer)
                 .expect("Could not generate rust code");
@@ -515,9 +328,10 @@ mod tests {
 
     #[test]
     fn count_schema() {
-        let openapi = load_openapi().spec;
+        let spec = load_openapi();
+        let componenets = spec.get_components().unwrap();
         let mut count = 0;
-        for (_, _) in openapi.components.unwrap().schemas.iter() {
+        for (_, _) in componenets.schemas.iter() {
             count += 1;
         }
 
