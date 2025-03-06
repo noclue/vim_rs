4d507d2f321e3f415c0cff852801384d077353fd
Kiril Karaatanassov <karaatanasov@gmail.com>
2025-03-06 22:36:47 +0200

    Add rough description of the goal

--- vim_build/src/main.rs
+++ vim_build/src/main.rs
@@ -5,8 +5,19 @@ mod vim_model;
 
 use generator::emit_vim_bindings;
 use std::{path::Path, time::Instant};
-// use crate::generator::load_openapi;
-// use crate::printer::Printer;
+
+/// Types whose hierarchies are pruned from the model. The listed types will be generated. Their
+/// descendants will not. This means that:
+/// 1. struct types for the descendants will not be emitted
+/// 2. traits for the listed types and their descendants will not be emitted
+/// 3. additional 2 fields will be added to the listed types 
+///     - type_name_ : String - holding the discriminator of the deserialized types 
+///     - extra_fields_ : HashMap<String, serde_json::Value> - holding the extra fields that are not
+///       part of the base type schema
+/// 4. deserialization for the descendant types will be handled by the parent type Visitor. The
+/// parent type Visitor will optionally accept the discriminator during creation to populate the
+/// correct type_name_ for the descendant types.
+const PRUNED_TYPES: [&str; 1] = ["MethodFault"];
 
 fn main() {
     let root_folder = Path::new("../vim_rs/src/");
